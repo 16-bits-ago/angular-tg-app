c81d833a40a4aea26ec38d5f8cbd4b5d6e369ebb
Vlad Kapustsin <kapustsin.u@titan-t.com>
2023-12-08 10:59:32 +0200

    created init shop pages

---
+++ src/app/pages/product/product.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html'
+})
+export class ProductComponent {
+
+}
---
+++ src/app/pages/shop/shop.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shop.component.html'
+})
+export class ShopComponent {
+
+}
