ff0e59ebad586c015d857e60920f73ccd34b0585
Vlad Kapustsin <kapustsin.u@titan-t.com>
2023-12-10 15:49:53 +0200

    added product list in shop page

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,12 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TelegramService } from '../../services/telegram.service';
 import { ProductsService } from '../../services/products.service';
+import { ProductListComponent } from '../../components/product-list/product-list.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ProductListComponent],
   templateUrl: './shop.component.html'
 })
 export class ShopComponent {
